4c5eed3d3dc319aa83496db2d7f89c1cd6e4ffa7
slumpthug <gangguchi58@gmail.com>
2023-05-08 23:45:00 +0600

    added form page and components

---
+++ components/formcomp/Formcomp.jsx
@@ -0,0 +1,139 @@
+"use client";
+import React, { useRef, useState } from "react";
+import css from './Formcomp-style.module.css';
+import Image from "next/image";
+import jacket from '../../public/form/jacket.png';
+import man from '../../public/form/man.jpg';
+import MainButtonTwo from "../main-button-two/MainButtonTwo";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+
+const Formcomp = () => {
+    return (
+        <div className={css.form}>
+            <div className={css.container}>
+                <div className={css.form__slider}>
+                  <Swiper
+                    cssMode={true}
+                    navigation={true}
+                    pagination={true}
+                    mousewheel={true}
+                    keyboard={true}
+                    modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                    className={css.swiper}
+                    style={{
+                      "--swiper-navigation-color": "#fff",
+                      "--swiper-pagination-color": "#fff",
+                    }}
+                  >
+                    <SwiperSlide>
+                      <Image className={css.man} src={man} alt="man" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image className={css.man} src={man} alt="man" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image className={css.man} src={man} alt="man" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image className={css.man} src={man} alt="man" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image className={css.man} src={man} alt="man" />
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
+                <div className={css.form__info}>
+                  <h1>ОБРАЗ В ДЕТАЛЯХ</h1>
+                  <div className={css.form__card_wrapper}>
+                        <div className={css.form__card}>
+                          <Image className={css.jacket} src={jacket} alt="jacket" />
+                          <div className={css.card__title}>
+                            <h4>
+                              Жилет с капюшоном и наполнителем из пуха Cortigiani
+                            </h4>
+                            <div className={css.card__sizes}>
+                              <button>50</button>
+                              <button>52</button>
+                              <button>54</button>
+                              <button>56</button>
+                            </div>
+                          </div>
+                            <div className={css.card__prices}>
+                              <span>123 000 ₽</span>
+                              <span>96 000 ₽</span>
+                            </div>
+                            <MainButtonTwo text='КУПИТЬ'/>
+                        </div>
+                        <div className={css.form__card}>
+                          <Image className={css.jacket} src={jacket} alt="jacket" />
+                          <div className={css.card__title}>
+                            <h4>
+                              Жилет с капюшоном и наполнителем из пуха Cortigiani
+                            </h4>
+                            <div className={css.card__sizes}>
+                              <button>50</button>
+                              <button>52</button>
+                              <button>54</button>
+                              <button>56</button>
+                            </div>
+                          </div>
+                            <div className={css.card__prices}>
+                              <span>123 000 ₽</span>
+                              <span>96 000 ₽</span>
+                            </div>
+                            <MainButtonTwo text='КУПИТЬ'/>
+                        </div>
+                        <div className={css.form__card}>
+                          <Image className={css.jacket} src={jacket} alt="jacket" />
+                          <div className={css.card__title}>
+                            <h4>
+                              Жилет с капюшоном и наполнителем из пуха Cortigiani
+                            </h4>
+                            <div className={css.card__sizes}>
+                              <button>50</button>
+                              <button>52</button>
+                              <button>54</button>
+                              <button>56</button>
+                            </div>
+                          </div>
+                            <div className={css.card__prices}>
+                              <span>123 000 ₽</span>
+                              <span>96 000 ₽</span>
+                            </div>
+                            <MainButtonTwo text='КУПИТЬ'/>
+                        </div>
+                        <div className={css.form__card}>
+                          <Image className={css.jacket} src={jacket} alt="jacket" />
+                          <div className={css.card__title}>
+                            <h4>
+                              Жилет с капюшоном и наполнителем из пуха Cortigiani
+                            </h4>
+                            <div className={css.card__sizes}>
+                              <button>50</button>
+                              <button>52</button>
+                              <button>54</button>
+                              <button>56</button>
+                            </div>
+                          </div>
+                            <div className={css.card__prices}>
+                              <span>123 000 ₽</span>
+                              <span>96 000 ₽</span>
+                            </div>
+                            <MainButtonTwo text='КУПИТЬ'/>
+                        </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Formcomp;
---
+++ src/pages/form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import BottomHeader from '../../components/bottom-header/BottomHeader';
+import Footer from '../../components/footer/Footer';
+import Formcomp from '../../components/formcomp/Formcomp';
+
+const form = () => {
+    return (
+        <div>
+            <Header/>
+            <BottomHeader/>
+            <Formcomp/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default form;
