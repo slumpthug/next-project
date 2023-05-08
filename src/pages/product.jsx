c76b7c27480a6e0362ee16f8e73ed42e0010e684
slumpthug <gangguchi58@gmail.com>
2023-05-08 23:57:39 +0600

    added product page and components

---
+++ src/pages/product.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import BottomHeader from '../../components/bottom-header/BottomHeader';
+import Footer from '../../components/footer/Footer';
+import Productcomp from '../../components/productcomp/Productcomp';
+
+const product = () => {
+    return (
+        <div>
+            <Header/>
+            <BottomHeader/>
+            <Productcomp/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default product;
