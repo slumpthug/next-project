a966ac233fda69c80ca4748ee7800a0873510219
slumpthug <gangguchi58@gmail.com>
2023-05-08 16:48:54 +0600

    added brands block

---
+++ components/brands/Brands.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import css from './Brands-style.module.css';
+import MainButton from '../main-button/MainButton';
+import Image from 'next/image';
+import logo from '../../public/brands/logo.svg';
+
+const Brands = () => {
+    return (
+        <div className={css.brands}>
+            <div className={css.container}>
+                <h1>ТОП-БРЕНДЫ</h1>
+                <div className={css.brands_wrapper}>
+                    <a href='#'>
+                        <Image className={css.logo} src={logo} alt="" />
+                    </a>
+                    <a href='#'>
+                        <Image className={css.logo} src={logo} alt="" />
+                    </a>
+                    <a href='#'>
+                        <Image className={css.logo} src={logo} alt="" />
+                    </a>
+                    <a href='#'>
+                        <Image className={css.logo} src={logo} alt="" />
+                    </a>
+                    <a href='#'>
+                        <Image className={css.logo} src={logo} alt="" />
+                    </a>
+                    <a href='#'>
+                        <Image className={css.logo} src={logo} alt="" />
+                    </a>
+                </div>
+                <MainButton text="все бренды"/>
+            </div>
+        </div>
+    );
+};
+
+export default Brands;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../../components/header/Header'
 import First from '../../components/first/First'
+import Brands from '../../components/brands/Brands'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <Header/>
       <First/>
+      <Brands/>
     </>
   )
 }
