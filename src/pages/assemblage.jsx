6e455e3b29ab632fdfc0fe7a31d42cf976f1b2cf
slumpthug <gangguchi58@gmail.com>
2023-05-12 00:05:32 +0600

    added assemblage page and component

---
+++ src/pages/assemblage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import BottomHeader from '../../components/bottom-header/BottomHeader';
+import Footer from '../../components/footer/Footer';
+import Assemblagecomp from '../../components/assemblagecomp/Assemblagecomp';
+
+const assemblage = () => {
+    return (
+        <div>
+            <Header/>
+            <BottomHeader text='Коллекция' />
+            <Assemblagecomp/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default assemblage;
