56d1a9f564f39e00fd3570d120ec892674291467
slumpthug <gangguchi58@gmail.com>
2023-05-15 17:53:39 +0600

    added demo filter

--- components/assemblagecomp/Assemblagecomp.jsx
+++ components/assemblagecomp/Assemblagecomp.jsx
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './Assemblage-style.module.css';
 import Privilege from '../privilege/Privilege';
 import CollectionCard from '../collection-card/CollectionCard';
 import man from '../../public/collection/man.jpg';
 
 const Assemblagecomp = () => {
+    let [category, setCategory] = useState('')
+
+    const card = [
+        {
+            id: 1,
+            text: 'Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni', 
+            cat: 1,
+        },
+        {
+            id: 2,
+            text: 'Пуховый жилет Kiton, джинсы Jacob Cohen, футболка Fedeli, кроссовки Valentino, бейсболка Kiton',
+            cat: 3,
+        },
+        {
+            id: 3,
+            text: 'aaaaaaaaaaaaaaaaaaaaaaa',
+            cat: 1,
+        },
+        {
+            id: 4,
+            text: 'bbbbbbbbbbbbbbbbbbbbbbbbbb',
+            cat: 3,
+        },
+        {
+            id: 5,
+            text: 'Пуховый жилет Kiton, джинсы Jacob Cohen, футболка Fedeli, кроссовки Valentino, бейсболка Kiton',
+            cat: 1,
+        },
+        {
+            id: 6,
+            text: 'Пуховый жилет Kiton, джинсы Jacob Cohen, футболка Fedeli, кроссовки Valentino, бейсболка Kiton',
+            cat: 3,
+        },
+    ]
+
     return (
         <div className={css.assemblagecomp}>
             <div className={css.container}>
                 <h1>КОЛЛЕКЦИЯ</h1>
                 <div className={css.assemblagecomp__filter}>
-                    <span>Сортировать</span>
-                    <div>по популярности</div>
+                    <button onClick={() => setCategory(1)}>по популярности</button>
+                    <button onClick={() => setCategory(0)}>все</button>
+                    <button onClick={() => setCategory(3)}>по цене</button>
                 </div>
                 <div className={css.assemblagecomp__wrapper}>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
-                    <CollectionCard link='/form' text='Спортивный костюм Artigiani, футболка Artigiani, кеды Santoni' img={man}/>
+                    {card.filter(item => item.cat == category ).map( item => {
+                        return (
+                            <>
+                                <CollectionCard link='/form' text={item.text} img={man} />
+                            </>
+                        )
+                    })}
                 </div>
                 <Privilege/>
             </div>
