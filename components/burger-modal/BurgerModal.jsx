cbfd6f731c61544b0bce0264c1be8ff35638e759
slumpthug <gangguchi58@gmail.com>
2023-05-21 20:41:55 +0600

    fixed bugs

--- components/burger-modal/BurgerModal.jsx
+++ components/burger-modal/BurgerModal.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
 import css from './BurgerModal-style.module.css';
 import Image from 'next/image';
 import close from '../../public/delivery-modal/close.svg';
+import { useRouter } from 'next/router';
+
+const navigation = [
+    { id: 1, title: 'каталог', path: '/catalog'},
+    { id: 1, title: 'санжар лох', path: '/loh'}
+];
 
 const BurgerModal = ({ isOpen, onClose }) => {
 
     if (!isOpen) {
         return null; // Если модальное окно закрыто, возвращаем null, чтобы ничего не отображать
     }
+
+    const { pathname } = useRouter();
 
     return (
         <div className={css.burgerModal} onClick={() => onClose(false)}>
@@ -20,9 +29,13 @@ const BurgerModal = ({ isOpen, onClose }) => {
                     </button>
                 </div>
                 <div className={css.burgerModal__wrapper}>
-                    <a href='/catalog' className={css.burgerModal__card}>
-                        каталог
-                    </a>
+                    {navigation.map(item => {
+                        return(
+                            <Link href={item.path} className={pathname == item.path ? css.burgerModal__card_active : css.burgerModal__card}>
+                                {item.title}
+                            </Link>
+                        )
+                    })}
                 </div>
             </div>
         </div>
--- components/main-card/MainCard.jsx
+++ components/main-card/MainCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 import css from './MainCard-style.module.css';
 import Image from 'next/image';
 
 const MainCard = ({img, title, price, proprice}) => {
     return (
-        <a href='/product' className={css.main__card}>
+        <Link href='/product' className={css.main__card}>
             <Image className={css.img} src={img} alt="bag" />
             <h5>{title}</h5>
             <div>
                 <span>{proprice}</span>
                 <span>{price}</span>
             </div>
-        </a>
+        </Link>
     );
 };
 
